77bd9ad72b2b8d6be0ffe508576563c85bc920c9
hemantksingh <hemantksingh.hk@gmail.com>
2019-03-12 12:19:15 +0000

    Use env vars to deploy function

--- Makefile
+++ Makefile
@@ -12,3 +12,13 @@ run: build
 	docker run -it --name $(CONTAINER) \
 		-e TARGET_ENV=$(TARGET_ENV) \
 		-v ~/.azure:/root/.azure $(IMAGE)
+
+deploy: build
+	docker rm -f $(CONTAINER) 2>/dev/null
+	docker run -it --name $(CONTAINER) \
+		-e TARGET_ENV=$(TARGET_ENV) \
+		-e AZURE_CLIENT_ID=$(AZURE_CLIENT_ID) \
+		-e AZURE_SECRET=$(AZURE_SECRET) \
+		-e AZURE_SUBSCRIPTION_ID=$(AZURE_SUBSCRIPTION_ID) \
+		-e AZURE_TENANT=$(AZURE_TENANT) \
+		$(IMAGE)
--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
-#!/bin/bash
+ #!/bin/bash -e
 
-echo Azure login
-source <(grep = ~/.azure/credentials)
-az login --service-principal -u $client_id -p $secret -t $tenant
+ # -e: exit right away on an error
+ # $?: get the exit status of the last command, exit status 0 -> success
 
-echo Deploying function
+if [[ -z "$AZURE_CLIENT_ID" ]] && [[ -z "$AZURE_SECRET"  ]] && [[ -z "$AZURE_TENANT" ]] ; then
+    echo "Azure Login using sourced ini creds..."
+    # source <(grep = ~/.azure/credentials)
+    az login --service-principal -u $client_id -p $secret -t $tenant
+else
+    echo "Azure Login using env vars..."
+    az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_SECRET -t $AZURE_TENANT
+fi
 
+echo Deploying function...
